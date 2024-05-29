6032eb2cac28ee49b1353d99f096b8c550b5350e
michaeljymsgutierrez <leeez711@gmail.com>
2024-05-29 11:06:26 +0800

    Auto push

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TestComponent from './components/test-component'
 
 export const ARM = new ApiResourceManager(['cities', 'areas'])
 
-ARM.setHost('https://www.metromart.com')
+ARM.setHost('https://team-staging.metromart.com')
 ARM.setHeadersCommon('Authorization', `Token ${window.localStorage.getItem('token')}`)
 ARM.setHeadersCommon('Content-Type', 'application/vnd.api+json')
 ARM.setHeadersCommon('X-Client-Platform', 'Web')
