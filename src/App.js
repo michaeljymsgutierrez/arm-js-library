04343d29281805174ec22c57737103cde9403da5
michaeljymsgutierrez <leeez711@gmail.com>
2024-06-03 18:44:39 +0800

    Auto push

--- src/App.js
+++ src/App.js
@@ -33,66 +33,74 @@ const App = observer(() => {
     <div className="App">
       <h3>Customer Addresses From Alias</h3>
       <table>
-        <tr>
-          <th>ID</th>
-          <th>ADDRESS1</th>
-          <th>ADDRESS2</th>
-          <th>KIND</th>
-          <th>ACTION</th>
-        </tr>
-
-        {customerAddresses.map((customerAddress, index) => (
-          <tr key={index}>
-            <td>{customerAddress.get('id')}</td>
-            <td>{customerAddress.get('attributes.address1')}</td>
-            <td>{customerAddress.get('attributes.address2')}</td>
-            <td>{customerAddress.get('attributes.kind')}</td>
-            <td>
-              <button onClick={() => customerAddress.save()}>
-                Save Record
-              </button>
-              <button>Delete Record</button>
-              <button
-                onClick={() => {
-                  ARM.unloadRecord(customerAddress)
-                }}
-              >
-                Unload Record
-              </button>
-            </td>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>ADDRESS1</th>
+            <th>ADDRESS2</th>
+            <th>KIND</th>
+            <th>ACTION</th>
           </tr>
-        ))}
+        </thead>
+
+        <tbody>
+          {customerAddresses.map((customerAddress, index) => (
+            <tr key={index}>
+              <td>{customerAddress.get('id')}</td>
+              <td>{customerAddress.get('attributes.address1')}</td>
+              <td>{customerAddress.get('attributes.address2')}</td>
+              <td>{customerAddress.get('attributes.kind')}</td>
+              <td>
+                <button onClick={() => customerAddress.save()}>
+                  Save Record
+                </button>
+                <button>Delete Record</button>
+                <button
+                  onClick={() => {
+                    ARM.unloadRecord(customerAddress)
+                  }}
+                >
+                  Unload Record
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
 
       <hr />
 
       <h3>Customer Addresses From Collection</h3>
       <table>
-        <tr>
-          <th>ID</th>
-          <th>ADDRESS1</th>
-          <th>ADDRESS2</th>
-          <th>KIND</th>
-          <th>ACTION</th>
-        </tr>
-
-        {addresses.map((address, index) => (
-          <tr key={index}>
-            <td>{address.get('id')}</td>
-            <td>{address.get('attributes.address1')}</td>
-            <td>{address.get('attributes.address2')}</td>
-            <td>{address.get('attributes.kind')}</td>
-            <td>
-              <button onClick={() => address.save()}>Save Record</button>
-              <button onClick={() => address.destroyRecord()}>
-                Delete Record
-              </button>
-              <button onClick={() => ARM.unloadRecord(address)}>
-                Unload Record
-              </button>
-            </td>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>ADDRESS1</th>
+            <th>ADDRESS2</th>
+            <th>KIND</th>
+            <th>ACTION</th>
           </tr>
-        ))}
+        </thead>
+
+        <tbody>
+          {addresses.map((address, index) => (
+            <tr key={index}>
+              <td>{address.get('id')}</td>
+              <td>{address.get('attributes.address1')}</td>
+              <td>{address.get('attributes.address2')}</td>
+              <td>{address.get('attributes.kind')}</td>
+              <td>
+                <button onClick={() => address.save()}>Save Record</button>
+                <button onClick={() => address.destroyRecord()}>
+                  Delete Record
+                </button>
+                <button onClick={() => ARM.unloadRecord(address)}>
+                  Unload Record
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   )
