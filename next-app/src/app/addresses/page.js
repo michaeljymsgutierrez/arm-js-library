5b330b42b1704d3772471798680c8d359d366fc8
michaeljymsgutierrez <leeez711@gmail.com>
2024-07-04 14:03:01 +0800

    Pushed for now next-app demo

---
+++ next-app/src/app/addresses/page.js
@@ -0,0 +1,11 @@
+'use client'
+
+const AddressesListPage = () => {
+  return (
+    <div className="addreses-list-page">
+      <h3>Address List Page</h3>
+    </div>
+  )
+}
+
+export default AddressesListPage
