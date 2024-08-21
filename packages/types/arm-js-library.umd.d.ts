566d5793cb0537ed6f05fe3dcfa13b8868e22250
Chael Gutierrez <leeez711@gmail.com>
2024-08-22 00:32:14 +0800

    features/expose-push-payload-function (#119)
    * Exposed pushPayload for manual pushing raw data to collections
    
    * Updated condition for pushPayload collection records type
    
    * Updated README for pushPayload function
    
    * Generated new DOCS.md
    
    * Generated declaration types

--- packages/types/arm-js-library.umd.d.ts
+++ packages/types/arm-js-library.umd.d.ts
@@ -213,6 +213,13 @@ declare module "arm-js-library.umd" {
              */
             _pushPayload(collectionName: string, collectionRecords: any[] | any): any[] | any;
             /**
+             * Pushes records to a specified collection.
+             *
+             * @param {string} collectionName - The name of the collection to push records to.
+             * @param {Array<Object>|Object} collectionRecords - The records to be pushed. Can be an array or a single object.
+             */
+            pushPayload(collectionName: string, collectionRecords: Array<any> | any): void;
+            /**
              * Pushes a request and its corresponding response to the request hash store.
              *
              * @private
