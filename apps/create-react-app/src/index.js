61bd678c23f993e28f2270a0ea86a06e069c3e8b
michaeljymsgutierrez <leeez711@gmail.com>
2024-09-30 18:18:09 +0800

    Updated demo apps

--- apps/create-react-app/src/index.js
+++ apps/create-react-app/src/index.js
@@ -6,7 +6,7 @@ import ApiResourceManager from 'arm-js-library'
 
 export const ARM = new ApiResourceManager(['addresses', 'users'])
 
-ARM.setHost('https://www.metromart.com')
+ARM.setHost('https://api.metromart.com')
 ARM.setHeadersCommon(
   'Authorization',
   `Token eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MTI5ODA4NjAsInR5cGUiOiJjdXN0b21lciIsInNob3AtaWQiOjIwNDgsImlhdCI6MTcxMjI4NDE5Mn0.I14uJ2CclmiqAfMniveoQT878Adn49t5ssmx9F9ewXE`
