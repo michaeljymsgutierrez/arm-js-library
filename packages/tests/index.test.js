69d97b4b6e4b36da7e3b882292b5d88b72ef2620
michaeljymsgutierrez <leeez711@gmail.com>
2024-11-04 14:05:22 +0800

    Added js dom for testing purposes

--- packages/tests/index.test.js
+++ packages/tests/index.test.js
@@ -1,7 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import ApiResourceManager from '../src'
 
 const ARM = new ApiResourceManager(['addresses'])
 
 test('Validate creation of address collection', () => {
+  console.log(window.ARM)
   expect(Object.keys(ARM.collections)).toContain('addresses')
 })
