b149f8a2bee35ba7d12d9da9f5481aa55d739ada
michaeljymsgutierrez <leeez711@gmail.com>
2024-11-04 17:37:16 +0800

    Added test for header setter

--- packages/tests/index.test.js
+++ packages/tests/index.test.js
@@ -2,17 +2,19 @@
  * @jest-environment jsdom
  */
 
+import axios from 'axios'
 import ApiResourceManager from '../src'
 
 const ARM = new ApiResourceManager(['addresses'])
 
+ARM.setHeadersCommon('X-Client-Platform', 'sailfish-os')
 ARM.setHost('https://api.arm-js-library.com')
 ARM.setNamespace('api/v2')
 ARM.setGlobal()
 
 describe('ARM: Instance initialization', () => {
   test('Verify address collection creation', () => {
-    expect(Object.keys(ARM.collections)).toContain('addresses')
+    expect(ARM.collections).toHaveProperty('addresses')
   })
 
   test('Verify global ARM instance', () => {
@@ -25,5 +27,9 @@ describe('ARM: Instance initialization', () => {
 
   test('Verify namespace value', () => {
     expect(ARM.namespace).toBe('api/v2')
+  })
+
+  test('Verify common headers setter', () => {
+    expect(axios.defaults.headers.common['X-Client-Platform']).toBe('sailfish-os')
   })
 })
