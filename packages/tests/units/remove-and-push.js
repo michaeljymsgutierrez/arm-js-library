c8b61504c3ba50af6f9f2ccdbe93116663f58058
michaeljymsgutierrez <leeez711@gmail.com>
2024-11-07 17:49:14 +0800

    Added pushPayload test

--- packages/tests/units/remove-and-push.js
+++ packages/tests/units/remove-and-push.js
@@ -17,6 +17,22 @@ const execRemoveAndPushTest = (ARM) => {
       expect(ARM.getCollection('addresses')).toHaveLength(11)
     })
   })
+
+  describe('Push collection record function', () => {
+    test('Verify pushPayload functionality', async () => {
+      ARM.clearCollection('addresses')
+      expect(ARM.getCollection('addresses')).toHaveLength(0)
+
+      const results = await ARM.ajax({
+        method: 'get',
+        url: 'addresses',
+      })
+
+      ARM.pushPayload('addresses', results.data.data)
+
+      expect(ARM.getCollection('addresses')).toHaveLength(12)
+    })
+  })
 }
 
 export default execRemoveAndPushTest
