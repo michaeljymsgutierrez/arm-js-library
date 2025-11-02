97b6e1186e6b8e7a9da34de55c0001b071435431
michaeljymsgutierrez <leeez711@gmail.com>
2025-11-02 20:17:46 +0800

    Added demo route for collection record state and properties

---
+++ apps/create-next-app/src/app/demo/collection-record/state-and-properties/page.jsx
@@ -0,0 +1,90 @@
+'use client'
+
+import { observer } from 'mobx-react-lite'
+import Model from './model'
+
+const Page = observer(() => {
+  const { isLoading, data: address } = Model()
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ADDRESS1</th>
+          <th>ADDRESS2</th>
+          <th>CITY</th>
+          <th>POST CODE</th>
+          <th>LANDMARK</th>
+          <th>KIND</th>
+          <th>LABEL</th>
+          <th>LONGITUDE</th>
+          <th>LATITUDE</th>
+        </tr>
+      </thead>
+      <tbody>
+        {isLoading && (
+          <tr>
+            <td colSpan="9">Loading...</td>
+          </tr>
+        )}
+
+        {!isLoading && (
+          <>
+            <tr>
+              <td>{address.get('attributes.address1')}</td>
+              <td>{address.get('attributes.address2')}</td>
+              <td>{address.get('attributes.city')}</td>
+              <td>{address.get('attributes.post-code')}</td>
+              <td>{address.get('attributes.landmark')}</td>
+              <td>{address.get('attributes.kind')}</td>
+              <td>{address.get('attributes.label')}</td>
+              <td>{address.get('attributes.longitude')}</td>
+              <td>{address.get('attributes.latitude')}</td>
+            </tr>
+
+            <tr>
+              <td colSpan="9">
+                <p>
+                  <b>isLoading:</b>&nbsp;
+                  {String(address?.isLoading)}
+                </p>
+
+                <p>
+                  <b>isError:</b>&nbsp;
+                  {String(address?.isError)}
+                </p>
+
+                <p>
+                  <b>isPristine:</b>&nbsp;
+                  {String(address?.isPristine)}
+                </p>
+
+                <p>
+                  <b>isDirty:</b>&nbsp;
+                  {String(address?.isDirty)}
+                </p>
+
+                <p className="flex flex-1 mt-5 gap-2">
+                  <button className="blue-btn" onClick={() => address.reload()}>
+                    Reload
+                  </button>
+
+                  <button
+                    className="blue-btn"
+                    onClick={() =>
+                      address.set('attributes.landmark', 'New landmark')
+                    }
+                  >
+                    Modify
+                  </button>
+                </p>
+              </td>
+            </tr>
+          </>
+        )}
+      </tbody>
+    </table>
+  )
+})
+
+export default Page
