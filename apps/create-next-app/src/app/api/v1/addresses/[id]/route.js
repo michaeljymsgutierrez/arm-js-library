120626034f6fc0d3a2ea2af7795df1dae16cbb81
michaeljymsgutierrez <leeez711@gmail.com>
2025-11-09 02:24:46 +0800

    Added dynamic address id API route

--- apps/create-next-app/src/app/api/v1/addresses/[id]/route.js
+++ apps/create-next-app/src/app/api/v1/addresses/[id]/route.js
@@ -4,30 +4,21 @@
 import addresses from '@/data/addresses'
 import users from '@/data/users'
 
-export async function GET(request) {
+export async function GET(request, { params }) {
+  const id = (await params).id
   const searchParams = request.nextUrl.searchParams
-  const include = searchParams.get('include')
+  const filteredAddresses = addresses.filter((address) => address.id == id)
+  const responsePayload = { data: filteredAddresses[0] ?? null }
 
-  if (include) {
-    return Response.json(
-      { data: addresses[0], included: users },
-      {
-        status: 200,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    )
-  }
-  return Response.json(
-    { data: addresses[0] },
-    {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  )
+  if (request.nextUrl.searchParams.get('include') === 'user')
+    responsePayload.included = [users[0]]
+
+  return Response.json(responsePayload, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
 }
 
 /**
