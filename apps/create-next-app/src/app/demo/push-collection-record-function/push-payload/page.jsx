bc86e8731a86ef30791866cc856bda0fbd22e82f
michaeljymsgutierrez <leeez711@gmail.com>
2026-01-08 23:52:25 +0800

    Added demo route for pushPayload

---
+++ apps/create-next-app/src/app/demo/push-collection-record-function/push-payload/page.jsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { observer } from 'mobx-react-lite'
+import Model from './model'
+import Controller from './controller'
+import { ARM } from '@/components/providers/arm-config-provider'
+
+const Page = observer(() => {
+  Model()
+  const controller = Controller()
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ADDRESS1</th>
+          <th>ADDRESS2</th>
+          <th>CITY</th>
+          <th>POST CODE</th>
+          <th>LANDMARK</th>
+          <th>KIND</th>
+          <th>LABEL</th>
+          <th>LONGITUDE</th>
+          <th>LATITUDE</th>
+        </tr>
+      </thead>
+      <tbody>
+        {controller.addresses.map((address) => (
+          <tr key={address.get('id')}>
+            <td>{address.get('attributes.address1')}</td>
+            <td>{address.get('attributes.address2')}</td>
+            <td>{address.get('attributes.city')}</td>
+            <td>{address.get('attributes.post-code')}</td>
+            <td>{address.get('attributes.landmark')}</td>
+            <td>{address.get('attributes.kind')}</td>
+            <td>{address.get('attributes.label')}</td>
+            <td>{address.get('attributes.longitude')}</td>
+            <td>{address.get('attributes.latitude')}</td>
+          </tr>
+        ))}
+        <tr>
+          <td colSpan="9">
+            <button className="blue-btn" onClick={controller.onPushRecord}>
+              Push Record (2518368)
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  )
+})
+
+export default Page
