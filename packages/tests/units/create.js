5a3fd2edf8e2d4ee1ce0c17b8f17c2a18250e185
michaeljymsgutierrez <leeez711@gmail.com>
2026-01-27 22:48:40 +0800

    Cleaned up unit tests

--- packages/tests/units/create.js
+++ packages/tests/units/create.js
@@ -1,8 +1,4 @@
-import { v1 as uuidv1 } from 'uuid'
-import { killConsole } from '../helpers'
-
 const execCreateTest = (ARM) => {
-  killConsole()
   ARM.setNamespace('api/v1')
 
   describe('Create collection record function', () => {
